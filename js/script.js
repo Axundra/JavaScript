75db430ffc5d62e73637fb673a7fb09ea759805d
axundra <axundra@hotmail.com>
2024-03-19 00:59:53 -0300

    Retoques al HTMl y CSS, JS funcional en gral., falta storage

--- js/script.js
+++ js/script.js
@@ -1,44 +1,57 @@
-let ingreso = "";
-let sesion = false;
-let pass = "";
-let user = "";
+let ingreso = "",
+    sesion = false,
+    pass = "",
+    user = "";
 const cuentas = [
     { cuenta: 'Jhon', contrasena: 'Salchichon' },
     { cuenta: 'Gustavo', contrasena: 'fideos' },
     { cuenta: 'Ariel', contrasena: 'uwu' },
     { cuenta: 'Gnomo', contrasena: 'wo!' },
 ];
+let loginForm = document.getElementById("form"), //usé let porque tengo que volver a asignarles valores al cambiar la estructura HTML
+    loginButt = document.getElementById("log"),
+    regButt = document.getElementById("reg"),
+    loginError = document.getElementById("error_txt");
 
+//↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ Variables
+//↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ HTML
+///////////////////////////////////
+//imagino que esto no se hace así//
+///////////////////////////////////
 
-/////////////////////////////////////////////////////////////Funciones
+const htmlReg = `   <input type="text" name="user" id="user" class="box" placeholder="Usuario"> 
+                    <input type="password" name="pass" id="pass_id" class="box" placeholder="Contraseña">
+                    <input type="password" name="pass2" id="pass2_id" class="box" placeholder="Confirmar contraseña">
+                    <input type="button" value="Registrarme" id="reg">
+                    <input type="button" value="Vovler a inicio de sesión" id="log">`,
+    //hoja de registro^
 
-function submit() {
+    htmlLog = `     <input type="text" name="user" id="user" class="box" placeholder="Usuario">
+                    <input type="password" name="pass" id="pass_id" class="box" placeholder="Contraseña">
+                    <input type="button" value="Iniciar sesión" id="log">
+                    <input type="button" value="Registrarse" id="reg">`,
+    //index^
 
-}
+    htmlError = `<p id="error_txt" style="opacity: 0;">Usuario y/o<span id="error">contraseña invalidos</span></p>`,
+    //error usuario y/o contraseña incorrectos^
 
+    htmlErrorReg = `<p id="error_txt">Usuario ya existente<span id="error">por favor, ingrese otro nombre de usuario</span></p>`,
+    //error usuario ya existente^
+    
+    htmlErrorRegPass = `<p id="error_txt">Las contraseñas<span id="error">no coinciden</span></p>`,
+    //error contraseñas diferentes^
 
+    htmlErrorRegNan = `<p id="error_txt">Complete todos los<span id="error">campos por favor</span></p>`;
+//error datos vacíos^
 
+//↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ HTML
+//↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ Funciones
 
-function crearCuenta(user_crear, pass_crear) {//crea usuario nuevo
-    //añade una caja de texto para confirmar contraseña y elimina un boton
-    document.getElementById('form').innerHTML = '<input type="text" name="user" id="user" class="box" placeholder="Usuario">   <input type="password" name="pass" id="pass_id" class="box" placeholder="Contraseña">   <input type="password" name="pass2" id="pass2_id" class="box" placeholder="Confirmar contraseña">    <input type="button" value="Registrarme" id="reg">'
-    //imagino que así no se hace pero no recuerdo otro método :c    
-    regButt = document.getElementById("reg");//tengo que volver a buscar el boton porque usé el .innerHTML arriba
-
-    const check = cuentas.find(element => {//busca si el usuario ya existe
-        return element.cuenta == user_crear;
-    })
-
-    regButt.addEventListener("click", () => {
-        if (check == null) {//checkea si el usuario no coincide con otro
-            cuentas.push({ cuenta: user_crear, contrasena: pass_crear })
-            iniciarSesion(user_crear, pass_crear)//ejecuta la funcion para iniciar sesión automaticamente
-        } else {
-            document.getElementById('error_box').innerHTML = '<p id="error_txt">Usuario ya existente<span id="error">por favor, ingrese otro nombre de usuario</span></p>'
-            alert('El usuario ya existe, por favor intente con otro nombre:')//para evitar problemas con el código
-        }
-    })
-
+function errorBox() {
+    loginError.style.opacity = 1;
+    setTimeout(()=>{
+    loginError.style.opacity = 0;
+    }, 3000)
 }
 
 
@@ -69,38 +82,73 @@ function recuperarPass() {//Busca el nombre de usuario y devuelve la contraseña
 
 function iniciarSesion(user_iniciar, pass_iniciar) {//por algun molesto motivo el forEach no funcionaba como debía y solo pasaba una vez por el array por más elementos que tuviera
     const cuentaCorrecta = cuentas.some(element => {      //busca el objeto correspondiente
-        return element.cuenta == user_iniciar && element.contrasena == pass_iniciar;
+        return element.cuenta === user_iniciar && element.contrasena === pass_iniciar;
     })
-    if (cuentaCorrecta) {       //chequea si se encontró el objeto
+    if (cuentaCorrecta) {       //chequea si se encontró la cuenta
+        //////////////////////////////////////////////////////////////////////////////////
         sesion = true;
+        //////////////////////////////////////////////////////////////////////////////////
     } else {                  //si no se encontró nada:
-        loginError.style.opacity = 1;
+        errorBox();
     }
 }
-/////////////////////////////////////////////////////////////Funciones
+
+//↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ Funciones
+//↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ Event Listeners
 
 
-/*addEventListener("change", patas)
-    function patas (){
-    document.getElementById("a").append(document.getElementById("nombre").value)
-} */
-
-
-let loginForm = document.getElementById("form"),
-    loginButt = document.getElementById("log"),
-    regButt = document.getElementById("reg"),
-    loginError = document.getElementById("error_txt");
-
-
+//login
 loginButt.addEventListener("click", () => {
-    console.log(loginButt.value);
     const user = loginForm.user.value,
         pass = loginForm.pass.value;
     iniciarSesion(user, pass)
 })
 
-regButt.addEventListener("click", () => {
-    crearCuenta();
+//registro
+regButt.addEventListener("click", () => {//crea usuario nuevo               no lo hice funcion porque no me pareció necesario
+    loginError.style.opacity = 0;//oculta el error en caso de estar
+
+    loginForm.innerHTML = htmlReg; //añade una caja de texto para confirmar contraseña
+
+    loginButt = document.getElementById("log");
+    regButt = document.getElementById("reg");           //tengo que volver a buscar todo porque usé el .innerHTML arriba
+    loginError = document.getElementById("error_txt");
+
+
+    regButt.addEventListener("click", () => {//click a "Registrarse"
+
+        const formPass = document.getElementById("form").pass.value,
+            formPass2 = document.getElementById("form").pass2.value,        //guarda los datos
+            formUser = document.getElementById("form").user.value;
+
+        const check = cuentas.find(element => {     //busca si el usuario ya existe
+            return element.cuenta == formUser;
+        })
+
+        if (formPass === "" || formPass2 === "" || formUser === "") {     //checkea hay datos en blanco
+            document.getElementById('error_box').innerHTML = htmlErrorRegNan;
+            errorBox();
+        } else {
+            if (check == null) {    //si se encuentra repetido el usuario en la 'base de datos' check no es null
+                if (formPass == formPass2) {    //chekea si las contraseñas son iguales
+                    cuentas.push({ cuenta: formUser, contrasena: formPass })
+                    console.log(cuentas);
+                    iniciarSesion(formUser, formPass)   //ejecuta la funcion para iniciar sesión automaticamente
+                } else {      //si las contraseñas no son iguales
+                    document.getElementById('error_box').innerHTML = htmlErrorRegPass;
+                    errorBox();
+                }
+            } else {
+                document.getElementById('error_box').innerHTML = htmlErrorReg;
+                errorBox();
+            }
+        }
+    })
+
+    loginButt.addEventListener("click", () => {
+
+    })
+
 })
 
 
